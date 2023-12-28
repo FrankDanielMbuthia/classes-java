beb5fc437650846c525c61554cba3f24407ca994
Frank Daniel <mbuthiawanganju@gmail.com>
2023-12-27 12:27:27 -1200

    removed some lines to check second commit

--- MyFirstKaffe/src/main.java
+++ MyFirstKaffe/src/main.java
@@ -12,9 +12,6 @@ public class main {
 	Circle c2=new Circle();
 	//System.out.println(Circle.getNumofCicles());
 	c2.setx(82);
-	System.out.println("I'm back better believe it!");
-	System.out.println("Just gimme some time. I won't give up on you so please,,, pleaaase dont give up on me!");
-		
 	//System.out.println(c1.getx());
 	
 	
